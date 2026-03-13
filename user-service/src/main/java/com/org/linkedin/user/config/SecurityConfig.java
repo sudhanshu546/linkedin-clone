d8ad845307823fac55cf4f37f20249bf7d10b9aa
sudhanshu546 <sudhanshusaini546@gmail.com>
2026-03-14 01:07:07 +0530

    spotless issue

--- user-service/src/main/java/com/org/linkedin/user/config/SecurityConfig.java
+++ user-service/src/main/java/com/org/linkedin/user/config/SecurityConfig.java
@@ -11,11 +11,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
-        return http.build();
-    }
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
+    return http.build();
+  }
 }
